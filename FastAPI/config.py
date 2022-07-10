b77f2f6bbceebff401826c6617fc3110bc94c9b7
Restyled.io <commits@restyled.io>
2022-07-10 17:42:40 +0000

    Restyled by black

--- FastAPI/config.py
+++ FastAPI/config.py
@@ -8,7 +8,7 @@ class Settings:
     POSTGRES_USER: str = str(os.getenv("DB_USER"))
     POSTGRES_PASSWORD: str = str(os.getenv("DB_PASSWORD"))
     POSTGRES_SERVER: str = str(os.getenv("DB_HOST"))
-    POSTGRES_PORT: int = int(os.getenv("DB_PORT"))   # type: ignore
+    POSTGRES_PORT: int = int(os.getenv("DB_PORT"))  # type: ignore
     POSTGRES_DB: str = str(os.getenv("DB_NAME"))
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
