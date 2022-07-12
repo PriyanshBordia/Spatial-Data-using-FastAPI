7485536e770da16a9e7972922b5e8be76e92fb7f
Restyled.io <commits@restyled.io>
2022-07-12 18:38:45 +0000

    Restyled by black

--- FastAPI/db/crud.py
+++ FastAPI/db/crud.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import Session
 
 def get_country_by_admin(db: Session, admin: str):
     try:
-        return db.query(
-            models.Country).filter(models.Country.admin == admin).all()
+        return db.query(models.Country).filter(models.Country.admin == admin).all()
     except Exception as e:
         raise e
