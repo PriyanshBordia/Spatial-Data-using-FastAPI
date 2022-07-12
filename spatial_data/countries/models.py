868816104fb27bf150969bb88526e479ad3f1132
PriyanshBordia <62763160+PriyanshBordia@users.noreply.github.com>
2022-07-13 00:02:19 +0530

    cleaning

--- spatial_data/countries/models.py
+++ spatial_data/countries/models.py
@@ -10,8 +10,8 @@ class Country(models.Model):
 	iso_a3 = models.CharField(max_length=255)
 	geom = models.PolygonField()
 
-    class Meta:
-        db_table = "polygon"
-        managed = False
-        verbose_name = "polygon"
-        verbose_name_plural = "polygons"
+	class Meta:
+		db_table = "polygon"
+		managed = False
+		verbose_name = "polygon"
+		verbose_name_plural = "polygons"
