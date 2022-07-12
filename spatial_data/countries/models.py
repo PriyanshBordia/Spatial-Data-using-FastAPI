6d3808df6f75d940ab09988bb69045dc89ec7b09
Restyled.io <commits@restyled.io>
2022-07-12 18:38:43 +0000

    Restyled by autopep8

--- spatial_data/countries/models.py
+++ spatial_data/countries/models.py
@@ -5,13 +5,13 @@ from django.contrib.gis.db import models
 
 class Country(models.Model):
 
-	ogc_fid = models.IntegerField()
-	admin = models.CharField(max_length=255)
-	iso_a3 = models.CharField(max_length=255)
-	geom = models.PolygonField()
+    ogc_fid = models.IntegerField()
+    admin = models.CharField(max_length=255)
+    iso_a3 = models.CharField(max_length=255)
+    geom = models.PolygonField()
 
-	class Meta:
-		db_table = "polygon"
-		managed = False
-		verbose_name = "polygon"
-		verbose_name_plural = "polygons"
+    class Meta:
+        db_table = "polygon"
+        managed = False
+        verbose_name = "polygon"
+        verbose_name_plural = "polygons"
