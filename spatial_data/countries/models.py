c0a124a182c4916a5b3255008f676a28716d6441
PriyanshBordia <62763160+PriyanshBordia@users.noreply.github.com>
2022-07-13 22:06:29 +0530

    updated db and models

--- spatial_data/countries/models.py
+++ spatial_data/countries/models.py
@@ -5,7 +5,12 @@ from django.contrib.gis.db import models
 
 class Country(models.Model):
 
-	ogc_fid = models.IntegerField()
-	admin = models.CharField(max_length=255)
-	iso_a3 = models.CharField(max_length=255)
-	geom = models.PolygonField()
+	admin = models.CharField(max_length=255, blank=False, null=False)
+	iso_a3 = models.CharField(max_length=255, blank=False, null=False)
+	geom = models.MultiPolygonField(blank=False, null=False)
+
+	def __str__(self):
+		return f"{self.id}. {self.admin} {self.iso_a3}"
+
+	class Meta:
+		ordering = ['admin']
