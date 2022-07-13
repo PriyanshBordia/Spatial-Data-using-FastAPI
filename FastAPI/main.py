f54c4d332df38958f5d0fa74e2271ac4a3b95bd8
Restyled.io <commits@restyled.io>
2022-07-13 17:27:05 +0000

    Restyled by reorder-python-imports

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,6 +1,11 @@
 from config import settings
-from db import crud, models, schemas, sessions
-from fastapi import Depends, FastAPI, HTTPException
+from db import crud
+from db import models
+from db import schemas
+from db import sessions
+from fastapi import Depends
+from fastapi import FastAPI
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=sessions.engine)
